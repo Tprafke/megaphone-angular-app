13f6a7d25f631d2a1c99215faa9bb4d7113a4ff5
garret <garretcabello@gmail.com>
2020-09-14 14:24:31 -0400

    i once again have worked on the master

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  showNav = false;
 
-  constructor() { }
+  constructor() {}
+  toggleNav = () => {
+    this.showNav = !this.showNav;
+  };
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
